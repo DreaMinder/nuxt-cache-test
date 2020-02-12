3720b1958394a32248a41a684dcebcb61134faf8
DreaMinder <dreaminder7@gmail.com>
2020-02-13 01:04:56 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    TEST
+  </section>
+</template>
+
+<style>
+section {
+  padding: 5rem;
+  text-align: center;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 200%;
+}
+</style>
