7fbcda9bdddbf9179850f0eed20e81119daae9c0
DreaMinder <dreaminder7@gmail.com>
2020-07-04 16:02:41 +0300

    tst3

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,12 @@
-console.log(Object.keys(process.env).filter(k => !k.includes('npm')))
+const vars = process.env
+const keys = Object.keys(process.env).filter(k => !k.includes('npm'))
+
+
+keys.forEach(k => {
+  console.log(vars[k])
+})
+
+
 module.exports = {
   head: {
     title: 'test'
